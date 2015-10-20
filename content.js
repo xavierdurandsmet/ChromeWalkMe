1c3291d4b02fda25ff7612fdbb4f01843507aa00
Xavier Durand-Smet <xavier.durand-smet>
2015-10-19 21:05:23 -0700

    JSONP working

--- content.js
+++ content.js
@@ -6,18 +6,34 @@ chrome.runtime.onMessage.addListener(
 
   	// loads data (Part II)
   	function loadData (userId) {
+  		console.log('get it')
   		// define webpage with userId
-		var url = 'https://s3.amazonaws.com/s3.maketutorial.com/users/' + userId + '/settings.txt'
-		$.getJSON('http://anyorigin.com/get?url='+ url, function(data){
-    		var JSONFile = data.contents.slice(14, data.contents.length - 2)
-    		console.log('JSONFile', JSONFile)
-    		// NOT WORKING!!!
-    		var finalData = JSON.parse(JSONFile)
-    		infoObject.libfile = finalData.LibFile;
-    		infoObject.dataFiles = finalData.DataFiles.length;
-    		infoObject.languages = finalData.languages.length;
-		});
-  	}
+		var url = 'https://s3.amazonaws.com/s3.maketutorial.com/users/' + infoObject.userId + '/settings.txt';
+		// create XMLHttpRequest
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", url, true);
+		xmlhttp.onreadystatechange = function() {
+	    	if (xmlhttp.readyState == 4) {
+	       		// get the JSONP from the URL
+	       		var JSONP = xmlhttp.responseText
+	       		// create a wrapper function with the callback name, to convert JSONP to JSON
+	        	var wrapper = new Function("fixedCallback", JSONP)
+	        	// convert JSONP to JSON
+	        	wrapper(function (json) {
+	        		// attach data we want to the info object
+	        		infoObject.libFile = json.LibFile;
+	        		infoObject.dataFilesNumber = json.DataFiles.length;
+	        		// infoObject.languagesNumber = json.DataFiles[0].languages.length
+	        		console.log(infoObject)
+	        		})
+		  		}
+		  	// put the response sender is this function because it is asynchronous
+	  	}
+			sendResponse({info: infoObject})
+			xmlhttp.send();
+
+}
+
 
   	// checks each script and stops the loop if finds a walkme script
   	$('script').each(function (index) {
@@ -44,24 +60,25 @@ chrome.runtime.onMessage.addListener(
   			// get the CDN
   			infoObject.host = arr[2];
 
+			// get env
+			if (arr[5] === 'test') {
+				infoObject.env = 'Test';
+			}
+			else {
+				infoObject.env = 'Production';
+			}
+
   			// check if there is users info
   			if (arr[3] === 'users') {
   				// get user id
   				infoObject.userId = arr[4];
 
-		  		// // get data from the settings file from specific user
-		  		// loadData(infoObject.userId)
-
-  				// get env
-  				if (arr[5] === 'test') {
-  					infoObject.env = 'Test';
-  				}
-  				else {
-  					infoObject.env = 'Production';
-  				}
+		  		var url = 'https://s3.amazonaws.com/s3.maketutorial.com/users/' + infoObject.userId + '/settings.txt'
+		  		infoObject.url = url;
+		  		// get data from the settings file from specific user
+		  		loadData(infoObject.userId)
   			}
   		}
-	  	sendResponse({info: infoObject})
 	  	return true;
   	})
   	return false;
@@ -69,16 +86,23 @@ chrome.runtime.onMessage.addListener(
 )
   		
 
+	// JSONP
+		 // $.ajax({
+   //          crossDomain: true,
+   //          type:"GET",
+   //          contentType: "application/json; charset=utf-8",
+   //          async:false,
+   //          url: url + "?callback=?",
+   //          data: {projectID:1},
+   //          dataType: "jsonp",                
+   //          jsonpCallback: 'fixedCallback',
+   //          // Work with the response
+		 //    success: function( response ) {
+		 //        console.log( response ); // server response
+		 //    }
+   //      });
 	// DRAFTS
 		// REGEX
   		// var notWalkMe = /^(.(?!stringToExclude))*$/i;
   		// console.log($(this)['context'].src.replace(notWalkMe, ''))
   		// console.log($(this)['context'].src)
-
-  		// XML
-  		// var xmlhttp = new XMLHttpRequest();
-		// xmlhttp.open("GET", url, true);
-		// xmlhttp.send();
-		// var data = JSON.parse(xmlhttp.responseText);
-		// var data = xmlhttp.responseText;
-		// console.log('xmlhttp ',xmlhttp.responseText)
--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ dragButton.onclick = function () {
   console.log('yes sir')
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {button: 'theButton'}, function(response) {
-      console.log(Object.keys(response.info).length)
+      console.log(Object.keys(response.info))
       // if the page doesn't contain the walkme
       if (Object.keys(response.info).length) {
         // enabled
